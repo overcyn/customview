a80c529f7cf02974d80ff639717b44c90a5cef67
Kevin Dang <overcyn@gmail.com>
2017-07-18 13:23:39 -0700

    Initial commit

---
+++ customview.go
@@ -0,0 +1,34 @@
+package customview
+
+import (
+	_ "gomatcha.io/bridge"
+	"gomatcha.io/matcha/paint"
+	"gomatcha.io/matcha/view"
+)
+
+type View struct {
+	*view.Embed
+	PaintStyle paint.Painter
+}
+
+// New returns either the previous View in ctx with matching key, or a new View if none exists.
+func New(ctx *view.Context, key string) *View {
+	if v, ok := ctx.Prev(key).(*View); ok {
+		return v
+	}
+	return &View{
+		Embed: ctx.NewEmbed(key),
+	}
+}
+
+// Build implements view.View.
+func (v *View) Build(ctx *view.Context) *view.Model {
+	painter := paint.Painter(nil)
+	if v.PaintStyle != nil {
+		painter = v.PaintStyle
+	}
+	return &view.Model{
+		NativeViewName: "github.com/overcyn/customview",
+		Painter:        painter,
+	}
+}
---
+++ ios/CustomView/CustomView/CustomView.m
@@ -0,0 +1,31 @@
+//
+//  CustomView.m
+//  CustomView
+//
+//  Created by Kevin Dang on 7/18/17.
+//  Copyright © 2017 Matcha. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
++ (void)load {
+    MatchaRegisterView(@"github.com/overcyn/customview", ^(MatchaViewNode *node){
+        return [[CustomView alloc] initWithViewNode:node];
+    });
+}
+
+- (id)initWithViewNode:(MatchaViewNode *)viewNode {
+    if ((self = [super initWithFrame:CGRectZero])) {
+        self.viewNode = viewNode;
+    }
+    return self;
+}
+
+- (void)setNode:(MatchaNode *)value {
+    _node = value;
+    self.backgroundColor = [UIColor redColor];
+}
+
+@end
